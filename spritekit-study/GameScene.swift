863d22a545a1f564cce2a4c44c8d54bd0023cfed
JIMIN KIM <jim4020key@gmail.com>
2022-07-01 11:05:28 +0900

    Add interactive falling boxes

--- spritekit-study/GameScene.swift
+++ spritekit-study/GameScene.swift
@@ -10,5 +10,15 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
         print("You are in the game scene!")
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
     }
 }
