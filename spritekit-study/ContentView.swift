7d0bd1374641b58c27f5fadd4fb80a5595f55dce
JIMIN KIM <jim4020key@gmail.com>
2022-07-01 10:13:41 +0900

    Add SpriteView using SpriteKit

--- spritekit-study/ContentView.swift
+++ spritekit-study/ContentView.swift
@@ -6,16 +6,20 @@
 //
 
 import SwiftUI
+import SpriteKit
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 216, height: 216)
+        scene.scaleMode = .fill
+        return scene
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    var body: some View {
+        SpriteView(scene: self.scene)
+            .frame(width: 256, height: 256)
+            .ignoresSafeArea()
     }
 }
---
+++ spritekit-study/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  spritekit-study
+//
+//  Created by kimjimin on 2022/07/01.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        print("You are in the game scene!")
+    }
+}
