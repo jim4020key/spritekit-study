720bf34ae222d71d1cac7fac36993be3bc78e556
JIMIN KIM <jim4020key@gmail.com>
2022-07-01 11:38:09 +0900

    Add sound effect and music

--- spritekit-study/GameScene.swift
+++ spritekit-study/GameScene.swift
@@ -63,6 +63,10 @@ class GameScene: SKScene {
         
         physicsWorld.gravity = .zero
         physicsWorld.contactDelegate = self
+        
+        let backgroundMusic = SKAudioNode(fileNamed: "background-music-aac.caf")
+        backgroundMusic.autoplayLooped = true
+        addChild(backgroundMusic)
     }
     
     func random() -> CGFloat {
@@ -99,6 +103,7 @@ class GameScene: SKScene {
         guard let touch = touches.first else {
             return
         }
+        run(SKAction.playSoundFileNamed("pew-pew-lei.caf", waitForCompletion: false))
         let touchLocation = touch.location(in: self)
         let projectile = SKSpriteNode(imageNamed: "projectile")
         projectile.position = player.position
