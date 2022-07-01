0a8fde7bc7530a191308cc4623a915a82aedb982
JIMIN KIM <jim4020key@gmail.com>
2022-07-01 10:06:09 +0900

    Initial Commit

---
+++ spritekit-study/spritekit_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  spritekit_studyApp.swift
+//  spritekit-study
+//
+//  Created by kimjimin on 2022/07/01.
+//
+
+import SwiftUI
+
+@main
+struct spritekit_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
